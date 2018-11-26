023af4c192907c10e95846ad6140a2797ae44580
mak-454 <ahmed.khan@oneconvergence.com>
2018-11-26 17:47:56 +0000

    small fixes

--- installer/install.sh
+++ installer/install.sh
@@ -1,17 +1,14 @@
 echo "cloning dkube-installer ...."
 git clone https://github.com/mak-454/dkube-install && cd dkube-install && git checkout installer_test
-
-echo "Cloning and checkout Done ............."
 sleep 1
 if [ -d "installer" ]; then
-	echo "Inside installer............"
-	ls
 	cd installer/
+	echo "Installing python dependancies ...."
 	pip3 install -r requirements.txt
 	sleep 1
 	./dkubectl deploy --pkg all --client_id ${CLIENT_ID} --client_secret ${CLIENT_SECRET}
 else
-	echo "Installer direcotry not found"
+	echo "Installer directory not found"
 	exit 1
 fi
 
