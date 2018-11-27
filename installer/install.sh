2befb81c4bd0a67ce761660bd21b79a3da7db7c1
mak-454 <ahmed.khan@oneconvergence.com>
2018-11-27 08:54:53 +0000

    unistall changes

--- installer/install.sh
+++ installer/install.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env sh
 echo "cloning dkube-installer ...."
 git clone https://github.com/mak-454/dkube-install && cd dkube-install && git checkout installer_test
 sleep 1
@@ -6,7 +7,11 @@ if [ -d "installer" ]; then
 	echo "Installing python dependancies ...."
 	pip3 install -r requirements.txt
 	sleep 1
-	./dkubectl deploy --pkg all --client_id ${CLIENT_ID} --client_secret ${CLIENT_SECRET}
+	if [ $1 = "install" ]; then
+	    ./dkubectl deploy --pkg all --client_id ${CLIENT_ID} --client_secret ${CLIENT_SECRET}
+	else
+	    ./dkubectl delete --pkg all
+	fi
 else
 	echo "Installer directory not found"
 	exit 1
